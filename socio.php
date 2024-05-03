7c7d29765943f5e982b476acb80cd391c0eb8e93
Fabio de Andrés <fabiosancristobal@gmail.com>
2024-05-03 22:20:54 +0200

    Botón editar

--- socio.php
+++ socio.php
@@ -74,6 +74,7 @@ $row = $socio->fetch_assoc();
     <div class="contenedor">
         <a <?php echo "href='chStatus.php?id=" . $row["id"] . "'" ?>><button class="action-button"><?php if($row["baja"]==1){echo "Dar de alta";}else{echo "Dar de baja";} ?></button></a>
         <a <?php echo "href='chfamily.php?id=" . $row["id"] . "'" ?>><button class="action-button">Cambiar de familia</button></a>
+        <a <?php echo "href='editSocio.php?id=" . $row["id"] . "'" ?>><button class="action-button">Editar</button></a>
     </div>
     </div>
 
