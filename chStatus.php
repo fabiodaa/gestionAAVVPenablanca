be8348c26dd5477789406be570ec7a4d4623ce1f
Fabio de Andrés <fabiosancristobal@gmail.com>
2024-05-03 22:51:33 +0200

    Pantalla baja

--- chStatus.php
+++ chStatus.php
@@ -36,45 +36,11 @@ $row = $socio->fetch_assoc();
 <body>
     <?php include 'header.php'; ?>
     <div>
-        <h1>Información de socio</h1>
+        <h1><?php if($row["baja"]==1){ echo "Estás seguro de que quieres volver a dar de alta a ".$row["nombre"]." ".$row["apellidos"].". Ten en cuenta que la familia debe estar dada de alta para que las cuotas se calculen adecuadamente.";} else{ echo "¿Seguro que quieres dar de baja a ".$row["nombre"]." ".$row["apellidos"]."?"; } ?></h1>
     </div>
     <div class="contenedor">
-        <div class="campo">
-            <p>Número:</p>
-            <p><?php echo $row["id"] ?></p>
-        </div>
-        <div class="campo">
-            <p>Apellidos:</p>
-            <p><?php echo $row["apellidos"] ?></p>
-        </div>
-        <div class="campo">
-            <p>Nombre:</p>
-            <p><?php echo $row["nombre"] ?></p>
-        </div>
-        <div class="campo">
-            <p>Fecha de nacimiento:</p>
-            <p><?php echo fechaFormato($row["fechaNacimiento"])." (".edad($row["fechaNacimiento"])." años)" ?></p>
-        </div>
-        <div class="campo">
-            <p>DNI:</p>
-            <p><?php echo $row["dni"] ?></p>
-        </div>
-        <div class="campo">
-            <p>Email:</p>
-            <p><?php echo $row["email"] ?></p>
-        </div>
-        <div class="campo">
-            <p>Familia:</p>
-            <p><?php echo $row["familia"] ?></p>
-        </div>
-        <div class="campo">
-            <p>Baja:</p>
-            <p><?php if($row["baja"]==1){echo "Sí";} else{echo "No";} ?></p>
-        </div>
-    <div class="contenedor">
-        <a <?php echo "href='chStatus.php?id=" . $row["id"] . "'" ?>><button class="action-button"><?php if($row["baja"]==1){echo "Dar de alta";}else{echo "Dar de baja";} ?></button></a>
-        <a <?php echo "href='chfamily.php?id=" . $row["id"] . "'" ?>><button class="action-button">Cambiar de familia</button></a>
-        <a <?php echo "href='editSocio.php?id=" . $row["id"] . "'" ?>><button class="action-button">Editar</button></a>
+        <a <?php echo "href='chStatus.php?id=" . $row["id"] . "'" ?>><button class="action-button">Cancelar</button></a>
+        <a <?php echo "href='chfamily.php?id=" . $row["id"] . "'" ?>><button class="action-button">Confirmar</button></a>
     </div>
     </div>
 
