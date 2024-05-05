20023ac14517bf0d912525c1784b315655fa2bf6
Fabio de Andrés <fabiosancristobal@gmail.com>
2024-05-05 22:05:01 +0200

    Botón para cambiar cabeza

--- familia.php
+++ familia.php
@@ -17,7 +17,20 @@ if (isset($_GET["id"])) {
     header("location: .");
 }
 
+if(isset($_GET["direccion"])&&isset($_GET["id"])){
+    $conexion -> query("UPDATE familia SET direccion='". $_GET["direccion"] ."' WHERE id=". $_GET["id"]);
+    header("location: familia.php?id=". $_GET["id"]);
+}
+
+if (isset($_GET["id"])) {
+    $familia = mysqli_query($conexion, "SELECT * FROM familia WHERE id=" . $_GET["id"]);
+
+} else {
+    header("location: .");
+}
+
 $row = $familia->fetch_assoc();
+$socios=mysqli_query($conexion,"SELECT * FROM socio WHERE familia=". $row["id"]."");
 $cabeza = mysqli_query($conexion, "SELECT nombre,apellidos FROM socio WHERE id=" . $row["principal"]);
 $cabeza = $cabeza->fetch_assoc();
 
@@ -44,17 +57,62 @@ $cabeza = $cabeza->fetch_assoc();
     <div class="datosFamilia">
         <h3><?php echo "Id: " . $row["id"] ?></h3>
         <h3><?php echo "Direccion: " . $row["direccion"] ?></h3>
-        <h3><?php echo "Cabeza de familia: " . $cabeza["nombre"] . " ".$cabeza["apellidos"] ?></h3>
-        <h3><?php if($row["baja"]==1) echo "Baja: Sí"; else echo "Baja: No"; ?></h3>
+        <h3><?php echo "Cabeza de familia: " . $cabeza["nombre"] . " " . $cabeza["apellidos"] ?></h3>
+        <h3><?php if ($row["baja"] == 1)
+            echo "Baja: Sí";
+        else
+            echo "Baja: No"; ?></h3>
     </div>
     <div class="contenedor">
-        <a <?php echo "href='chStatusFamilia.php?id=" . $row["id"] . "'" ?>><button
-                class="action-button"><?php if ($row["baja"] == 1) {
-                    echo "Dar de alta";
-                } else {
-                    echo "Dar de baja";
-                } ?></button></a>
-        <a <?php echo "href='editFamilia.php?id=" . $row["id"] . "'" ?>><button class="action-button">Editar</button></a>
+        <a <?php echo "href='chStatusFamilia.php?id=" . $row["id"] . "'" ?>><button class="action-button"><?php if ($row["baja"] == 1) {
+                   echo "Dar de alta";
+               } else {
+                   echo "Dar de baja";
+               } ?></button></a>
+    </div>
+    <div>
+        <h3>Editar direccion</h3>
+        <form method="get" action="familia.php">
+            <div>
+                <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
+                <input required class="campoDireccion" <?php echo 'value="' . $row["direccion"] . '"'; ?> placeholder="Dirección"
+                    type="text" name="direccion" id="direccion">
+            </div>
+            <div>
+                <input class="confirmar" type="submit" value="Confirmar">
+            </div>
+        </form>
+    </div>
+    <div class="listaSocios">
+        <h3>Lista de socios</h3>
+        <?php
+            if($socios->num_rows > 0){
+                echo "<table class=\"socios\" border='1'>";
+                echo "<tr>";
+                echo "<th>Nº</th>";
+                echo "<th>Apellidos</th>";
+                echo "<th>Nombre</th>";
+                echo "<th>Fecha nacimiento</th>";
+                echo "<th>Ver</th>";
+                echo "<th>Hacer cabeza de familia</th>";
+                echo "</tr>";
+                while($socio = $socios->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $socio["id"] . "</td>";
+                    echo "<td>" . $socio["apellidos"] . "</td>";
+                    echo "<td>" . $socio["nombre"] . "</td>";
+                    echo "<td>" . fechaFormato($socio["fechaNacimiento"])." (".edad($socio["fechaNacimiento"])." años)" . "</td>";
+                    echo "<td><a href='socio.php?id=" . $socio["id"] . "'><button class=\"ver\">Ver</button></a></td>";
+                    if($socio["id"]==$row["principal"]) echo "<td>Ya lo es</td>";
+                    elseif($socio["baja"]==1) echo "<td>Está de baja</td>";
+                    else echo "<td><a href='chCabeza.php?id=" . $socio["id"] . "&familia=". $row["id"] ."'><button class=\"principal\">Hacer cabeza de familia</button></a></td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }else{
+                ?> <h3>No hay resultados</h3><?php
+            }
+        ?>
     </div>
 
 
